9b06ec506bce25b8566459f57eaec2eae1849878
fraser-ran <fraserrankin@cmail.carleton.ca>
2024-10-22 07:59:31 -0400

    maing

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"unicode/utf8"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	var intNum int = 10
+	var floatNum float64 = 10.2
+
+	fmt.Println("int: ",intNum, "\n", "float: ", floatNum )
+
+	str:= "beans"
+	fmt.Println(utf8.RuneCountInString((str)))
+
+	
 }
